d3e89d289502918dbbdb311ea486544fe3fd9079
Brandon Chung <chung.brandon123@gmail.com>
2024-05-16 06:01:48 -0500

    Update C# API

---
+++ HeartHealthAPI/Models/MedicalRecords.cs
@@ -0,0 +1,17 @@
+﻿namespace HeartHealthAPI.Models
+{
+    public readonly record struct MedicalRecords(int age, int anemia, int creatine_phosphokinase, int ejection_fraction, int platelets, float serumCreatinine, int serumSodium, int daysUntilFollowUp)
+    {
+        /*
+         * class PatientMedicalRecords(BaseModel):
+            age: int
+            anemia: int
+            creatine_phosphokinase: int
+            ejection_fraction: int
+            platelets: int
+            serum_creatinine: float
+            serum_sodium: int
+            days_until_follow_up: int
+         */
+    }
+}
---
+++ HeartHealthAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace HeartHealthAPI.Models
+{
+    public class User
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
